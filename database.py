c6d69fda1c942f03086bf8cd3faa8320c460e816
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-07-05 14:53:09 +0530

    firebase credentials

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ cred = credentials.Certificate(
     {
         "type": "service_account",
         "projectId": os.getenv("FIREBASE_PROJECT_ID"),
-        "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
         "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
         "token_uri": "https://oauth2.googleapis.com/token",
     }
